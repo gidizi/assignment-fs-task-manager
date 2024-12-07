541b2654797f505d7d72aad67c7a638941d20a5a
Gidiz <gidizimmer@gmail.com>
2024-12-07 20:53:59 +0200

    chore: improving validation schemas

--- client/src/components/TaskForm/schema.ts
+++ client/src/components/TaskForm/schema.ts
@@ -1,12 +1,13 @@
 import { z } from 'zod';
 
 //Todo: reuse server schema
+//production: make regex to validate dueDate.
 export const taskSchema = z.object({
-    title: z.string(),
-    description: z.string(),
+    title: z.string().min(2, "Title must be at least 2 characters").max(30, "Title must not exceed 30 characters"),
+    description: z.string().max(100, "Description must not exceed 100 characters"),
     dueDate: z.string(),
     status: z.enum(['To Do', 'In Progress', 'Completed']),
-    taskOwner: z.string(),
+    taskOwner: z.string().min(2, "Task Owner must be at least 2 characters").max(30, "Task Owner must not exceed 30 characters"),
     priority: z.enum(['Low', 'Medium', 'High', 'Critical']),
 });
 
--- server/src/schemas/task.ts
+++ server/src/schemas/task.ts
@@ -1,12 +1,13 @@
 import { z } from 'zod';
 //todo: get enum value from shared type that will be used for mongo schema
+//production: make regex to validate dueDate.
 export const TaskSchema = z.object({
     id: z.number(),
-    title: z.string(),
-    description: z.string(),
+    title: z.string().min(2, "Title must be at least 2 characters").max(30, "Title must not exceed 30 characters"),
+    description: z.string().max(100, "Description must not exceed 100 characters"),
     dueDate: z.string(),
     status: z.enum(['To Do', 'In Progress', 'Completed']),
-    taskOwner: z.string(),
+    taskOwner: z.string().min(2, "Task Owner must be at least 2 characters").max(30, "Task Owner must not exceed 30 characters"),
     priority: z.enum(['Low', 'Medium', 'High', 'Critical']),
     tags: z.array(z.string()),
 });
