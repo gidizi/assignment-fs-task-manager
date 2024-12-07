3bb70a6027929fe510d09582ded78329d12f7f52
Gidiz <gidizimmer@gmail.com>
2024-12-07 15:49:41 +0200

    feat: task form takes care of edit

--- client/src/components/AddTask/AddTask.tsx
+++ client/src/components/AddTask/AddTask.tsx
@@ -1,9 +1,9 @@
-import { useForm } from 'react-hook-form'
+import { useForm, Controller } from 'react-hook-form'
 import TextField from '@mui/material/TextField';
 import MenuItem from '@mui/material/MenuItem';
 import axiosInstance from '../../API/axiosInstance';
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import Alert from '@mui/material/Alert';
 
 
@@ -12,30 +12,65 @@ import Alert from '@mui/material/Alert';
 //todo: get potential owners
 
 //todo: reuse from shared types library
-export interface ITask {
+interface ITask {
     title: string;
     description: string;
     dueDate: string;
     status: 'To Do' | 'In Progress' | 'Completed';
-    creationTime: Date;
     taskOwner: string;
     priority: 'Low' | 'Medium' | 'High' | 'Critical';
     tags: string[];
 }
 
+interface props {
+    taskId: string
+}
 
-//add editmode or taskId
-const AddTask = () => {
+const filterNonITaskKeys = (obj) => {
+    const allowedKeys: (keyof ITask)[] = ["title", "description", "dueDate", "status", "taskOwner", "priority", "tags"];
+    return Object.fromEntries(
+        Object.entries(obj).filter(([key]) => allowedKeys.includes(key as keyof ITask))
+    );
+}
+
+const AddTask = ({ taskId }): props => {
     const [error, setError] = useState('')
-    const { register, formState: { errors }, handleSubmit } = useForm()
+    const { register, formState: { errors }, handleSubmit, control, reset } = useForm()
+
+
+    useEffect(() => {
+        const fetchTask = async () => {
+            try {
+                const currentTask = await axiosInstance.get(`/tasks/${taskId}`)
+                const resData = currentTask?.data
+                console.log("resData", resData)
+                //note: in Prod app we would have set server data dto and converter for the form required format (BO)
+                reset(resData)
+            } catch (error) {
+                setError('Unexpected error has occured, please try to refresh the page ')//todo adjust
+            }
+        }
+        if (taskId) fetchTask()
+    }, [taskId])
     //todo: change undefined type
     const onSubmit = async (data: ITask) => {
         try {
-            const createdTask = await axiosInstance.post('/tasks', data)
-            const resData = createdTask?.data
+            if (taskId) {
+                const modifiedTask = await axiosInstance.put(`/tasks/${taskId}`, data)
+                const resData = modifiedTask?.data.data
+                const filteredObject = filterNonITaskKeys(resData)
+                reset(filteredObject)
+                //todo: update store
+
+            } else {
+                const createdTask = await axiosInstance.post('/tasks', data)
+                const resData = createdTask?.data
+                //todo: update store
+
+            }
             setError('')
-            //todo: do something with the data
         } catch (error) {
+            console.log(error)
             //todo: get a shared type of response type for both BE and FE!
             if (axios.isAxiosError(error)) {
                 setError(error.response?.data?.message?.message)
@@ -81,41 +116,57 @@ const AddTask = () => {
                     helperText={errors.dueDate?.message?.toString() || errors.dueDate?.type?.toString()}
                     fullWidth
                 />
-                <TextField
-                    select
-                    label="Priority"
-                    {...register("priority")}
-                    aria-invalid={errors.priority ? "true" : "false"}
-                    error={!!errors.priority}
-                    helperText={errors.priority?.message?.toString() || errors.priority?.type?.toString()}
-                    fullWidth>
-                    {(["Low", "Medium", "High", "Critical"] as ITask["priority"][]).map(option => (<MenuItem key={option} value={option}>{option}</MenuItem>))}
-                </TextField>
-                {/* {editMode && <TextField
-                    select
-                    label="Status"
-                    {...register("status")}
-                    aria-invalid={errors.status ? "true" : "false"}
-                    error={!!errors.status}
-                    helperText={errors.status?.message?.toString() || errors.status?.type?.toString()}
-                    fullWidth>
-                    {['new', 'old', 3].map(option => (<MenuItem key={option} value={option}>{option}</MenuItem>))}
-                </TextField>} */}
-                <TextField
-                    select
-                    label="Status"
-                    {...register("status")}
-                    aria-invalid={errors.status ? "true" : "false"}
-                    error={!!errors.status}
-                    helperText={errors.status?.message?.toString() || errors.status?.type?.toString()}
-                    fullWidth>
-                    {(['To Do', 'In Progress', 'Completed'] as ITask["status"][]).map(option => (<MenuItem key={option} value={option}>{option}</MenuItem>))}
-                </TextField>
+                <Controller
+                    name="priority"
+                    control={control}
+                    defaultValue="Low" // Set an initial value to avoid uncontrolled warnings
+                    render={({ field }) => (
+                        <TextField
+                            select
+                            label="Priority"
+                            {...field}
+                            fullWidth
+                            aria-invalid={errors.priority ? "true" : "false"}
+                            error={!!errors.priority}
+                            helperText={errors.priority?.message?.toString() || errors.priority?.type?.toString()}
+                        >
+                            {(["Low", "Medium", "High", "Critical"] as ITask["priority"][]).map((option) => (
+                                <MenuItem key={option} value={option}>
+                                    {option}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                    )}
+                />
+                <Controller
+                    name="status"
+                    control={control}
+                    defaultValue="To Do"
+                    render={({ field }) => (
+                        <TextField
+                            select
+                            label="Status"
+                            {...field}
+                            fullWidth
+                            disabled={!taskId}
+                            error={!!errors.status}
+                            helperText={errors.status?.message?.toString() || errors.status?.type?.toString()}
+                            aria-invalid={errors.status ? "true" : "false"}
+                        >
+                            {(['To Do', 'In Progress', 'Completed'] as ITask["status"][]).map((option) => (
+                                <MenuItem key={option} value={option}>
+                                    {option}
+                                </MenuItem>
+                            ))}
+                        </TextField>
+                    )}
+                />
+
 
 
                 <input type="submit" />
                 {error && <Alert severity="error">{error}</Alert>}
-            </form>
+            </form >
         </>
     )
 }
